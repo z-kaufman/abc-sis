631f816b2a4d7d7fe840bcbc2852ad9b9ec204be
Nathan Moore <nathan.moore@nrel.gov>
2024-11-25 17:37:23 -0700

    write a more useful csv file as output

--- aqi_scraper.py
+++ aqi_scraper.py
@@ -1,6 +1,7 @@
 import csv
 import logging
 from datetime import datetime
+from pathlib import Path
 from zoneinfo import ZoneInfo
 
 import httpx
@@ -10,6 +11,7 @@ _log = logging.getLogger(__name__)
 
 data_type = 'list'
 MOUNTAIN_TIMEZONE = ZoneInfo("America/Denver")
+UNITS = "µg/m³"
 
 url = "https://www.iqair.com/us/usa/utah/salt-lake-city"
 
@@ -39,35 +41,36 @@ def scrape_website(url: str, data_type: str) -> list | str:
         data = find_substring_and_context(soup.get_text(), 'PollutantsConcentration')
 
         if data is not None:
+            # End at the check-mark, no matter how much text we scooped up
             data = data.split("✓")[0]
+
+            # Separate the data into our desired bits
             data_as_list = data.split()
             data_as_list = [x.split("!")[0] for x in data_as_list[1:]]
-            _log.debug(data_as_list)
+
+            # split out values from units
+            more_split_data_list = []
+            for elem in data_as_list:
+                more_split_data_list.append(elem.split("\u00b5")[0])
+
+            metrics_list = []  # ['PM2.5', 'PM10', 'O₃', 'NO₂', 'SO₂', 'CO']
+            values_list = []  # [4.5, 16.0, 56.0, 25.5, 0.4, 229.0]
+            for elem in more_split_data_list:
+                try:
+                    values_list.append(float(elem))
+                except ValueError:
+                    if elem[0].isupper():
+                        metrics_list.append(elem)
+                    else:
+                        raise SystemExit(f"I don't know what this is: {elem}")
         else:
             raise SystemExit("No text returned from website")
-        return data_as_list
+        return metrics_list, values_list
     elif data_type == 'raw':
         data = soup.get_text()
         return data
     else:
         raise SystemExit("Incorrect or no desired data type provided. Must be either 'raw' or 'list'.")
-
-    # Extract data using Beautiful Soup methods
-    # Example: Find all paragraph tags
-    # tables = soup.find_all('table', class_='aqi-overview-detail__main-pollution-table')
-    # data = soup.prettify()
-    # text = soup.get_text()
-    # pm_25 = soup.find_all('PollutantsConcentration')
-    # print(pm_25)
-    # raise SystemExit()
-    # print(soup.prettify())
-
-    # Process and store the extracted data
-    # data = [p.text for p in paragraphs]
-    # data = [table.text for table in tables]
-    # print(data)
-
-    # return data_as_list
 
 
 def find_substring_and_context(long_string: str, substring: str, context_length: int = 100) -> str | None:
@@ -97,58 +100,42 @@ def find_substring_and_context(long_string: str, substring: str, context_length:
     return result
 
 
-# def save_to_csv(data, filename):
-#     with open(filename, 'w', newline='', encoding='utf-8') as file:
-#         writer = csv.writer(file)
-#         writer.writerows(data)
-
-
-# def save_to_json(data, filename):
-#     with open(filename, 'w', encoding='utf-8') as f:
-#         json.dump(data, f, ensure_ascii=False, indent=2)
-
-
 def save_to_txt(data, filename):
     with open(filename, 'w') as f:
         f.write(data)
 
 
-def save_list_to_csv(formatted_data, output_file):
+def save_list_to_csv(formatted_metrics, formatted_values, output_file):
+    write_header = True
+    if Path(output_file).exists():
+        write_header = False
+
     # Write to CSV
     with open(output_file, 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(["Mountain time", "Pollutant", "Concentration"])  # Write header
-        writer.writerows(formatted_data)
+        if write_header:
+            writer.writerow(formatted_metrics)  # Write header
+        writer.writerow(formatted_values)
 
 
-def organize_for_csv(input_list):
-    # Ensure the list has an even number of elements
-    if len(input_list) % 2 != 0:
-        input_list.append("")  # Add an empty string if odd number of elements
-
-    # Pair the elements to make a 2-column list for writing to csv
-    paired_data = list(zip(input_list[0::2], input_list[1::2]))
-
+def organize_for_csv(metrics_list, values_list):
     # Format the datetime to include only up to the hour
     mountain_time = datetime.now(MOUNTAIN_TIMEZONE)
-    formatted_datetime = mountain_time.strftime("%Y-%m-%d %H:00:00")
+    formatted_datetime = str(mountain_time.strftime("%Y-%m-%d %H:00:00"))
 
-    # Adding the current datetime to each tuple
-    formatted_data = [(formatted_datetime, x[0], x[1]) for x in paired_data]
+    values_list.insert(0, formatted_datetime)
+    metrics_list = [f"{x}{UNITS}" for x in metrics_list]
+    metrics_list.insert(0, "Timestamp")
 
-    return formatted_data
+    return metrics_list, values_list
 
-
-# Call the scraping function
-scraped_data = scrape_website(url, data_type)
-
-formatted_data = organize_for_csv(scraped_data)
 
 # After scraping
 if data_type == 'raw':
-    save_to_txt(scraped_data, f"test_text_{datetime.now()}.txt")
-# save_to_csv(scraped_data, f"aqi_{datetime.now()}.csv")
-# save_to_json(scraped_data, 'first_test.json')
-# save_to_csv(scraped_data, 'string_test.txt')
+    data = scrape_website(url, data_type)
+    save_to_txt(data, f"test_text_{datetime.now(MOUNTAIN_TIMEZONE)}.txt")
 elif data_type == 'list':
-    save_list_to_csv(formatted_data, "aqi_data.csv")
+    # Call the scraping function
+    metrics_list, values_list = scrape_website(url, data_type)
+    formatted_metrics, formatted_values = organize_for_csv(metrics_list, values_list)
+    save_list_to_csv(formatted_metrics, formatted_values, "aqi_data_v2.csv")
